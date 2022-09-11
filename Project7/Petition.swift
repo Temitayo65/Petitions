80815aa033b9d766b7253463488c301b1fb6c8a8
temitayo65 <iloritemitayo65@gmail.com>
2022-09-11 01:46:37 +0100

    First Version

---
+++ Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by ADMIN on 11/09/2022.
+//
+
+import Foundation
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+    
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,14 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by ADMIN on 11/09/2022.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
+
+
